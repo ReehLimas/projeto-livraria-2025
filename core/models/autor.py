3462987fa4d23e64b26e6dfce91ad86e08df5c96
ReehLimas <reehlimasil08@gmail.com>
2025-05-20 16:49:58 -0300

    feat: criação da model autor

---
+++ core/models/autor.py
@@ -0,0 +1,10 @@
+import email
+from django.db import models
+
+
+class Autor(models.Model):
+    nome = models.CharField(max_length=100)
+    email = models.EmailField(max_length=100)
+
+    def __str__(self):
+        return f"({self.id}) {self.nome.upper()}"
